a87aafcf438811caae4a9873e86e97213e45375f
Vaishnavi <vaishnavig61@gmail.com>
2024-07-23 13:11:32 +0530

    ex_22072024

---
+++ ex02_july/ex_22072024/test_lab157.py
@@ -0,0 +1,32 @@
+import pytest
+import allure
+
+@allure.title("TC#1-Verify that 2-2 is equal to 0")
+@allure.description("This is a smoke test case- Verify that 2-2 is equal to 0")
+@pytest.mark.smoke
+def test_sub0():
+    assert 2-2==0
+
+@allure.title("TC#2-Verify that 3-3 is equal to 0")
+@allure.description("This is a smoke test case- Verify that 3-3 is equal to 0")
+@pytest.mark.regression
+def test_sub1():
+    assert 3-3==0
+
+@allure.title("TC#3-Verify that 1-1 is equal to 0")
+@allure.description("This is a smoke test case- Verify that 1-1 is equal to 0")
+@pytest.mark.smoke
+def test_sub2():
+    assert 1-1==0
+
+@allure.title("TC#4-Verify that 0-0 is equal to 0")
+@allure.description("This is a smoke test case- Verify that 0-0 is equal to 0")
+@pytest.mark.sanity
+def test_sub3():
+    assert 0-0==0
+
+@allure.title("TC#5-Verify that 0-0 is not equal to 0")
+@allure.description("This is a smoke test case- Verify that 0-0 is not equal to 0")
+@pytest.mark.skip
+def test_sub4():
+    assert  0-0!=0
